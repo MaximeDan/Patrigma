a9c0ed9c6d37161c0ae65fdd0eb42bbc925adf56
alexandre-kakal-akarah <alexandre.kakal@akarah.com>
2024-06-18 19:50:03 +0200

    fix: navbar

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -1,24 +1,48 @@
+"use client";
 import Link from "next/link";
 import React from "react";
 import { Icons } from "./Icons";
+import { usePathname } from "next/navigation";
 
 const MobileNav = () => {
+  const currentPath = usePathname();
+  const isMainPage = currentPath === "/" || currentPath === "";
   return (
     <nav className="fixed inset-x-5 bottom-5 flex  items-end justify-center rounded-xl bg-background p-2">
       <Link
         href="/parcours"
         className="flex h-10  flex-1 items-center justify-center "
       >
-        <Icons.flag className="size-[30px]" fill="var(--light-gray)" />
+        <Icons.flag
+          className="size-[30px]"
+          fill={
+            currentPath.includes("/parcours")
+              ? "var(--orange)"
+              : "var(--light-gray)"
+          }
+        />
       </Link>
       <Link
         href="/"
         className="mx-auto flex h-10 flex-1 items-center justify-center "
       >
-        <Icons.compass className="size-[34px]" fill="var(--light-gray)" />
+        <Icons.compass
+          className="size-[34px]"
+          fill={isMainPage ? "var(--orange)" : "var(--light-gray)"}
+        />
       </Link>
-      <Link href="/" className="flex h-10 flex-1 items-center justify-center ">
-        <Icons.agenda className="size-[28px]" fill="var(--light-gray)" />
+      <Link
+        href="/evenements"
+        className="flex h-10 flex-1 items-center justify-center "
+      >
+        <Icons.agenda
+          className="size-[28px]"
+          fill={
+            currentPath.includes("/evenements")
+              ? "var(--orange)"
+              : "var(--light-gray)"
+          }
+        />
       </Link>
     </nav>
   );
