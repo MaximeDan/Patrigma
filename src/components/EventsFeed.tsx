dbcfd96ef3d8c195df9dc01a083d5392e168bb96
alexandre-kakal-akarah <alexandre.kakal@akarah.com>
2024-06-20 21:03:25 +0200

    fix: fetch url

--- src/components/EventsFeed.tsx
+++ src/components/EventsFeed.tsx
@@ -2,9 +2,12 @@ import EventAccordion from "./EventAccordion";
 import { Suspense } from "react";
 
 const EventsFeed = async () => {
-  const res = await fetch(`${process.env.BASE_URL}/api/events`, {
-    cache: "no-cache",
-  });
+  const res = await fetch(
+    `${process.env.BASE_URL || "http://localhost:3000"}/api/events`,
+    {
+      cache: "no-cache",
+    },
+  );
   const events = await res.json();
   return (
     <Suspense fallback={<div>Loading...</div>}>
