85bce10d8d013e7137e6d06a9c9cde8e699455e7
GuillaumeD <guillaume.deman@viacesi.fr>
2024-06-24 16:15:43 +0200

    Gde setup pwa (#16)
    Set up PWA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,52 @@
 import React from "react";
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const APP_NAME = "Patrigma";
+const APP_DEFAULT_TITLE = "Patrigma";
+const APP_TITLE_TEMPLATE = "%s - Patrigma";
+const APP_DESCRIPTION = "Découvre ton patrimoine en jouant !";
+
 export const metadata: Metadata = {
-  title: "Patrigma",
-  description: "",
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: "#FFFFFF",
 };
 
 export default function RootLayout({
