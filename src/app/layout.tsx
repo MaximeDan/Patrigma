161f861b24074bcb94d6fb928f7d981016542417
Guillaume DEMAN <guillaume22@hotmail.fr>
2024-06-24 15:29:45 +0200

    WIP

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -56,10 +57,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr">
-      <head>
+      <Head>
         <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#000000" />
+        <link rel="icon" href="/icons/icon-192x192.png" />
         {/* Ajoutez d'autres meta tags ici si nécessaire */}
-      </head>
+      </Head>
       <body className={`${inter.className} min-h-screen bg-background`}>
         {children}
       </body>
