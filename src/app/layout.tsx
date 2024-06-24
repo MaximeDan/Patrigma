aa68d6307806b2b74e824c92b79004984ecad3ec
Guillaume DEMAN <guillaume22@hotmail.fr>
2024-06-24 15:48:46 +0200

    Fix Layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -57,13 +56,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr">
-      <Head>
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#000000" />
-        <link rel="icon" href="/icons/icon-192x192.png" />
-        {/* Ajoutez d'autres meta tags ici si nécessaire */}
-      </Head>
       <body className={`${inter.className} min-h-screen bg-background`}>
         {children}
       </body>
