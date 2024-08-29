bdbd6f6c38651892dae3d2c86f4172f28dcb3d8e
Maxime Dandel <maxime.dandel@suez.com>
2024-08-29 08:32:09 +0200

    Revert "demo"
    This reverts commit 3f5321f33a82bf76141bce36f0ce5616b2ead016.

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -28,7 +28,7 @@ const TopBar = () => {
             href="/"
             className={`mx-auto flex h-10 flex-1 items-center justify-center font-bold uppercase ${isMainPage ? "text-orange" : "text-foreground"}`}
           >
-            COUCOU les loulous
+            Accueil
           </Link>
           <Link
             href="/parcours"
